82d7988234f83b400643087419b091c30c6f9296
Sarah Brown <srb@ou.edu>
2021-08-25 11:16:14 -0500

    meets current requirements with only one led on at once (may go back and redo with interrupts)

--- LabAssignment2/src/LabAssignment2.c
+++ LabAssignment2/src/LabAssignment2.c
@@ -16,7 +16,7 @@
 #define GREEN 1
 #define BLUE 2
 
-#define PRESSED 0
+#define PRESSED 0 // active low
 
 #define NUM_RED_LEDS 4
 #define NUM_GREEN_LEDS 4
@@ -33,7 +33,12 @@ const unsigned char p2_score_leds[] = {6,5}; //p2
 const unsigned char p1_switches[] = {12,11,10}; //p2.8, 2.7, 2.6
 const unsigned char p2_switches[] = {4,3,2}; //p2.8, 2.7, 2.6
 
-unsigned int leds_state = 0b1111111111110000; // rgbp1p2
+unsigned int leds_state = 0b1111111111110000; // rgbp1p2, leds start state so rgb are on, score is off
+
+typedef struct {
+
+} port_details;
+
 //y = 2.727*10^-6 * x + 9.2*10^-6, y in usec, x = ticks
 void wait_ticks(int ticks) {
 	volatile int count;
@@ -66,34 +71,40 @@ void change_led(const unsigned char led, volatile unsigned int * reg_pointer, in
 	}
 }
 
+void only_one(int led_count, int which_led, const unsigned char led, volatile unsigned int * reg_pointer, int is_high) {
+	if (led_count == which_led) {
+		change_led(led, reg_pointer, is_high);
+	}
+
+	else {
+		change_led(led, reg_pointer, FALSE);
+	}
+}
+
 unsigned int read_state(const unsigned char switches[], volatile unsigned int * reg_pointer, int color) {
 	int state_one = *reg_pointer & (1 << switches[color]);
-	wait_ticks(500); //about 1ms
+	wait_ticks(100);
 	int state_two = *reg_pointer & (1 << switches[color]);
-	wait_ticks(500);
-	int state_three = *reg_pointer & (1 << switches[color]);
-	return (state_one & state_two & state_three); // anded together for debounce
+	return (state_one & state_two); // anded together for debounce
 }
 
 void change_leds(int led_count) {
-	switch(led_count) {
-		case 0: change_led(p1_score_leds[0], FIO2PIN, leds_state & (1 << 0)); break;
-		case 1: change_led(p1_score_leds[1], FIO2PIN, leds_state & (1 << 1)); break;
-		case 2: change_led(p2_score_leds[0], FIO2PIN, leds_state & (1 << 2)); break;
-		case 3: change_led(p2_score_leds[1], FIO2PIN, leds_state & (1 << 3)); break;
-		case 4: change_led(blue_leds[0], FIO0PIN, leds_state & (1 << 4)); break;
-		case 5: change_led(blue_leds[1], FIO0PIN, leds_state & (1 << 5)); break;
-		case 6: change_led(blue_leds[2], FIO0PIN, leds_state & (1 << 6)); break;
-		case 7: change_led(blue_leds[3], FIO0PIN, leds_state & (1 << 7)); break;
-		case 8: change_led(green_leds[0], FIO0PIN, leds_state & (1 << 8)); break;
-		case 9: change_led(green_leds[1], FIO0PIN, leds_state & (1 << 9)); break;
-		case 10: change_led(green_leds[2], FIO0PIN, leds_state & (1 << 10)); break;
-		case 11: change_led(green_leds[3], FIO0PIN, leds_state & (1 << 11)); break;
-		case 12: change_led(red_leds[0], FIO0PIN, leds_state & (1 << 12)); break;
-		case 13: change_led(red_leds[1], FIO0PIN, leds_state & (1 << 13)); break;
-		case 14: change_led(red_leds[2], FIO0PIN, leds_state & (1 << 14)); break;
-		case 15: change_led(red_leds[3], FIO0PIN, leds_state & (1 << 15)); break;
-	}
+	only_one(led_count,  0, p1_score_leds[0], FIO2PIN, leds_state & (1 << 0));
+	only_one(led_count,  1, p1_score_leds[1], FIO2PIN, leds_state & (1 << 1));
+	only_one(led_count,  2, p2_score_leds[0], FIO2PIN, leds_state & (1 << 2));
+	only_one(led_count,  3, p2_score_leds[1], FIO2PIN, leds_state & (1 << 3));
+	only_one(led_count,  4, blue_leds[0], FIO0PIN, leds_state & (1 << 4));
+	only_one(led_count,  5, blue_leds[1], FIO0PIN, leds_state & (1 << 5));
+	only_one(led_count,  6, blue_leds[2], FIO0PIN, leds_state & (1 << 6));
+	only_one(led_count,  7, blue_leds[3], FIO0PIN, leds_state & (1 << 7));
+	only_one(led_count,  8, green_leds[0], FIO0PIN, leds_state & (1 << 8));
+	only_one(led_count,  9, green_leds[1], FIO0PIN, leds_state & (1 << 9));
+	only_one(led_count, 10, green_leds[2], FIO0PIN, leds_state & (1 << 10));
+	only_one(led_count, 11, green_leds[3], FIO0PIN, leds_state & (1 << 11));
+	only_one(led_count, 12, red_leds[0], FIO0PIN, leds_state & (1 << 12));
+	only_one(led_count, 13, red_leds[1], FIO0PIN, leds_state & (1 << 13));
+	only_one(led_count, 14, red_leds[2], FIO0PIN, leds_state & (1 << 14));
+	only_one(led_count, 15, red_leds[3], FIO0PIN, leds_state & (1 << 15));
 }
 
 void setup() {
